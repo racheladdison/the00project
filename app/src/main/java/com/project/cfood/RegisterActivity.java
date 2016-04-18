08671b2a22ba68fec12735167f892416cb3c9071
Ryphon <dylanshepard@me.com>
2016-04-17 20:32:36 -0600

    creating splash screen, with preliminary aws login code

--- app/src/main/java/com/project/cfood/RegisterActivity.java
+++ app/src/main/java/com/project/cfood/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.project.cfood;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -102,6 +103,10 @@ public class RegisterActivity extends AppCompatActivity implements LoaderCallbac
         mLoginFormView = findViewById(R.id.login_form);
     }
 
+    public void returnToLogin(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
     private void populateAutoComplete() {
         if (!mayRequestContacts()) {
             return;
