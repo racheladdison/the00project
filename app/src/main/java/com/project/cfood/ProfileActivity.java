34568aff3ecd8ca7917babd9bb8b30fa265d5f76
leif-waldner <leif.waldner@colorado.edu>
2016-04-25 11:51:31 -0600

    Merging with Master

--- app/src/main/java/com/project/cfood/ProfileActivity.java
+++ app/src/main/java/com/project/cfood/ProfileActivity.java
@@ -3,6 +3,7 @@ package com.project.cfood;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -14,18 +15,31 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
     private ListView profileListView ;
     private ArrayAdapter<String> listAdapter ;
     private TextView profileEmailView;
     private TextView profileUsernameView;
     private ImageView profileImageView;
     private DBCreator dbCreator;
+    private GoogleSignInResult result;
+    private GoogleSignInAccount acct;
+    private GoogleApiClient mGoogleAPIClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +55,17 @@ public class ProfileActivity extends AppCompatActivity {
         profileUsernameView = (TextView) findViewById( R.id.username);
         profileImageView = (ImageView) findViewById( R.id.userphoto);
 
+        //Access google signin
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestScopes(new Scope(Scopes.PLUS_LOGIN))
+                .requestEmail()
+                .build();
+
+        mGoogleAPIClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
         //TODO replace this with a database call that creates a list of event objects
         //String[] events = new Event()
         ArrayList<String> EventList = new ArrayList<String>();
@@ -54,9 +79,29 @@ public class ProfileActivity extends AppCompatActivity {
         //profileEmailView.setText();
         //profileUsernameView.setText();
         //profileImageView.setImage();
-
-
     }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleAPIClient);
+        startActivityForResult(signInIntent, 9001);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 9001){
+            result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            acct = result.getSignInAccount();
+        }
+    }
+
+    public String getUserID() {
+        signIn();
+        return acct.getId();
+    }
+
+
     public void EditProfile() {
         //Edit Profile
         //set on click listener for button? Or is that coded into the button itself?
@@ -91,4 +136,9 @@ public class ProfileActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
