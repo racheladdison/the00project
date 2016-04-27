1d6fd31629ff6bd96653e0e36c7c67cae55361dd
leif-waldner <leif.waldner@colorado.edu>
2016-04-27 00:58:26 -0600

    Changed things

--- app/src/main/java/com/project/cfood/ProfileActivity.java
+++ app/src/main/java/com/project/cfood/ProfileActivity.java
@@ -6,6 +6,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.util.Log;
+import android.view.MenuItem;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -17,7 +25,7 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import java.util.ArrayList;
 
 
-public class ProfileActivity extends AppCompatActivity{
+public class ProfileActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private ListView profileListView ;
     private ArrayAdapter<String> listAdapter ;
     private TextView profileEmailView;
@@ -67,6 +75,14 @@ public class ProfileActivity extends AppCompatActivity{
             }
         });
         //profileImageView.setImageResource();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) this);
+
     }
 
     public ArrayList<String> getEventList(UserClass user, EventTableHandler eventTable) {
@@ -76,4 +92,49 @@ public class ProfileActivity extends AppCompatActivity{
         }
         return eventList;
     }
+    public void toForum(View view) {
+        Intent intent = new Intent(this, Forum.class);
+        startActivity(intent);
+    }
+
+    public void toMap(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+
+    public void toProfile(View view) {
+        Intent intent = new Intent(this, ProfileActivity.class)
+        startActivity(intent);
+    }
+    @Override
+    public void onBackPressed() {
+       DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+                drawer.closeDrawer(GravityCompat.START);
+            } else {
+               super.onBackPressed();
+            }
+        }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        View view = findViewById(id);
+
+        if (id == R.id.nav_forum_view) {
+            toForum(view);
+        } else if (id == R.id.nav_map_view) {
+            toMap(view);
+        } else if (id == R.id.nav_profile) {
+            toProfile(view);
+        } else if (id == R.id.nav_sign_out) {
+
+            }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 }
