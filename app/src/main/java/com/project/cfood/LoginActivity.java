e4fb22d6faf385ef10a1c2b4b50b79784ac642de
rachellewis <rachlewis0000@gmail.com>
2016-04-26 22:35:30 -0600

    remove merge notation

--- app/src/main/java/com/project/cfood/LoginActivity.java
+++ app/src/main/java/com/project/cfood/LoginActivity.java
@@ -74,7 +74,6 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-<<<<<<< HEAD
 
         //Declare Toolbar
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
